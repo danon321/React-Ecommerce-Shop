b3a8945e9be91843fed7310cacaa0080e0d013bf
Daniel <44358467+danon321@users.noreply.github.com>
2023-04-17 22:21:34 +0200

    Create basic pages and add router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,30 @@
-function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/products/:id",
+      element: <Products />,
+    },
+    {
+      path: "/product/:id",
+      element: <Product />,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
-}
+};
 
 export default App;
