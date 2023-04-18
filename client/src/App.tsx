044aa2bc1cdd14cc440fba10fad547ad6db15f29
Daniel <44358467+danon321@users.noreply.github.com>
2023-04-18 15:21:29 +0200

    Add Layout and css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,45 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+import "./App.scss";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/products/:id",
-      element: <Products />,
-    },
-    {
-      path: "/product/:id",
-      element: <Product />,
-    },
-  ]);
-
   return (
     <div>
       <RouterProvider router={router} />
